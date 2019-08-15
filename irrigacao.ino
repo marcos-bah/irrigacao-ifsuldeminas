93de2a7c6ce7b431812f85e88cdc05c4e35d2175
Marcos Bah <49887610+marcos-bah@users.noreply.github.com>
2019-08-15 15:52:33 -0300

    Update irrigacao.ino

--- irrigacao.ino
+++ irrigacao.ino
@@ -7,23 +7,28 @@
  *Verificar se o relé é ativado em nível LOW (0V) ou HIGH (5V).
  */
 
-int valvula = 8; // pino válvula (no caso será um rele para a valvula solenoide)
-int sensor = A0;
-int horas = 2; // intervalo de verificacao
+#define valvula 8; // pino válvula (no caso será um rele para a valvula solenoide)
+#define sensorUmidade A0;
+#define horas 2; // intervalo de verificacao
+
+#define sensorMovimento = 9; // pino para sensor de movimento
  
 void setup() {
   pinMode(valvula, OUTPUT); // declara válvula como saída
+  Serial.begin(9600); // ativa serial monitor
 }
  
 void loop() {
  
- int sensorValue = analogRead(sensor); //lê valor de pino analogico A0
+ int leituraUmidade = analogRead(sensorUmidade); //lê valor de pino analogico A0
+ int leituraMovimento = digitalRead(sensorMovimento); //lê valor de pino digital 9
   
  //sensorValue varia entre 0 (umido) e 1023 (seco)
  //valor impirico
  
- while(sensorValue > 600){
+ while(leituraUmidade > 600){
    digitalWrite(valvula,LOW); // enquanto estiver seco, ativa a valvula 
+   if()
  }
    
  digitalWrite(valvula,HIGH); // desativa a valvula
@@ -31,3 +36,4 @@ void loop() {
  delay(1000*60*60*horas); // tempo de espera para checar umidade  
 
 }
+
